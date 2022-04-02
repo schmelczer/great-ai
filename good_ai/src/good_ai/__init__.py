c01d55d291989cb7ed77cdcb2252ed51bcc83384
András Schmelczer <andras@schmelczer.dev>
2022-04-02 21:59:09 +0200

    Format
    Signed-off-by: András Schmelczer <andras@schmelczer.dev>

--- good_ai/src/good_ai/__init__.py
+++ good_ai/src/good_ai/__init__.py
@@ -1,3 +1,3 @@
 from .good_ai import *
-from .utilities import *
 from .open_s3 import *
+from .utilities import *
--- good_ai/src/good_ai/good_ai/models/load_model.py
+++ good_ai/src/good_ai/good_ai/models/load_model.py
@@ -2,6 +2,7 @@ import logging
 from typing import Any, Optional
 
 from joblib import load
+
 from good_ai.open_s3 import LargeFile
 
 logger = logging.getLogger("models")
