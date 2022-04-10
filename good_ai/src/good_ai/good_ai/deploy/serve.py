6b2b4dc9899d984e6883ee889cd6d9f57676da53
András Schmelczer <andras@schmelczer.dev>
2022-04-10 19:52:04 +0200

    Add charts
    Signed-off-by: András Schmelczer <andras@schmelczer.dev>

--- good_ai/src/good_ai/good_ai/deploy/serve.py
+++ good_ai/src/good_ai/good_ai/deploy/serve.py
@@ -2,15 +2,10 @@ from typing import Any, Callable
 
 import uvicorn
 from fastapi import FastAPI, status
-from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi.responses import RedirectResponse
 
-from good_ai.good_ai.deploy.create_fastapi_app import create_fastapi_app
-
-from ..context import get_context
-from ..metrics import create_dash_app
 from ..tracing import TracingContext
 from ..views import Trace
+from .create_fastapi_app import create_fastapi_app
 
 
 def serve(
@@ -19,15 +14,9 @@ def serve(
     disable_metrics: bool = False,
     configure: Callable[[FastAPI], None] = lambda _: None,
 ) -> None:
-    app = create_fastapi_app(function.__name__, disable_docs=disable_docs)
-
-    if not disable_metrics:
-        dash_app = create_dash_app(function.__name__)
-        app.mount(get_context().metrics_path, WSGIMiddleware(dash_app))
-
-        @app.get("/", include_in_schema=False)
-        def redirect_to_entrypoint() -> RedirectResponse:
-            return RedirectResponse("/metrics")
+    app = create_fastapi_app(
+        function.__name__, disable_docs=disable_docs, disable_metrics=disable_metrics
+    )
 
     @app.post("/score", status_code=status.HTTP_200_OK, response_model=Trace)
     def process(input: Any) -> Trace:
--- good_ai/src/good_ai/good_ai/views/__init__.py
+++ good_ai/src/good_ai/good_ai/views/__init__.py
@@ -2,5 +2,6 @@ from .filter import Filter
 from .health_check_response import HealthCheckResponse
 from .model import Model
 from .operators import operators
+from .query import Query
 from .sort_by import SortBy
 from .trace import Trace
