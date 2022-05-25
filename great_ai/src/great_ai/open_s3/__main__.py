fbfb7ab1cec59615ceb1852d1995214ac908dec2
András Schmelczer <andras@schmelczer.dev>
2022-05-25 22:54:40 +0200

    Improve CLI DX

--- great_ai/src/great_ai/open_s3/__main__.py
+++ great_ai/src/great_ai/open_s3/__main__.py
@@ -7,8 +7,10 @@ from great_ai.utilities.logger import create_logger
 from .large_file import LargeFile
 from .parse_arguments import parse_arguments
 
-if __name__ == "__main__":
-    logger = create_logger("open_s3")
+logger = create_logger("open_s3")
+
+
+def main() -> None:
     parser, args = parse_arguments()
 
     LargeFile.configure_credentials_from_file(args.secrets)
@@ -16,22 +18,27 @@ if __name__ == "__main__":
     if not args.cache and not args.push and not args.delete:
         logger.warning("No action required.")
         parser.print_help()
+
+    if args.cache:
+        for c in args.cache:
+            split = c.split(":")
+            file_name = split[0]
+            version = None if len(split) == 1 else int(split[1])
+            LargeFile(file_name, "r", version=version).get()
+
+    if args.push:
+        for p in args.push:
+            path = Path(p)
+            LargeFile(path.name, "w").push(path)
+
+    if args.delete:
+        for f in args.delete:
+            LargeFile(f).delete()
+
+
+if __name__ == "__main__":
     try:
-        if args.cache:
-            for c in args.cache:
-                split = c.split(":")
-                file_name = split[0]
-                version = None if len(split) == 1 else int(split[1])
-                LargeFile(file_name, "r", version=version).get()
-
-        if args.push:
-            for p in args.push:
-                path = Path(p)
-                LargeFile(path.name, "w").push(path)
-
-        if args.delete:
-            for f in args.delete:
-                LargeFile(f).delete()
+        main()
     except KeyboardInterrupt:
         logger.warning("Exiting")
         exit()
---
+++ great_ai/src/great_ai/parse_arguments.py
@@ -0,0 +1,55 @@
+from argparse import ArgumentParser, Namespace
+
+
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser(
+        description="GreatAI-Server for deploying you AI applications with ease.",
+    )
+
+    parser.add_argument(
+        "file_name",
+        type=str,
+        help="the name of the file containing your to-be-served function such as `main.py`\n",
+    )
+
+    parser.add_argument(
+        "--function_name",
+        type=str,
+        help="name of your inference function, defaults to the base name of the filename or the literal `app`",
+        default="",
+        required=False,
+    )
+
+    parser.add_argument(
+        "--host",
+        type=str,
+        help="it is passed to uvicorn which starts a server listening on this address",
+        default="0.0.0.0",
+        required=False,
+    )
+
+    parser.add_argument(
+        "--port",
+        type=int,
+        help="it is passed to uvicorn which starts a server listening on this port",
+        default=6060,
+        required=False,
+    )
+
+    parser.add_argument(
+        "--timeout_keep_alive",
+        type=int,
+        help="it is passed to uvicorn which uses it for timing out requests taking longer than this many seconds",
+        default=600,
+        required=False,
+    )
+
+    parser.add_argument(
+        "--workers",
+        type=int,
+        help="it is passed to uvicorn which starts this many server processes",
+        default=1,
+        required=False,
+    )
+
+    return parser.parse_args()
