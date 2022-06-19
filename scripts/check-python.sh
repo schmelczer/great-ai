8f4a32070264cbe1cbb55ddcad25120505d48dce
Andras Schmelczer <andras@schmelczer.dev>
2022-06-19 17:03:40 +0200

    Fix scripts when there are only notebooks

--- scripts/check-python.sh
+++ scripts/check-python.sh
@@ -13,9 +13,13 @@ python3 -m autoflake --expand-star-imports --remove-all-unused-imports --ignore-
 python3 -m isort --profile black --skip .env . --check
 python3 -m black . --exclude .env --check
 
-yes | python3 -m mypy . --install-types > /dev/null || true
-python3 -m mypy --namespace-packages --ignore-missing-imports --install-types --non-interactive --disallow-untyped-defs --disallow-incomplete-defs --follow-imports=silent --exclude=external/ --exclude=/build/ --pretty .
+if ls *.py 1> /dev/null 2>&1; then
+    yes | python3 -m mypy . --install-types > /dev/null || true
+    python3 -m mypy --namespace-packages --ignore-missing-imports --install-types --non-interactive --disallow-untyped-defs --disallow-incomplete-defs --follow-imports=silent --exclude=external/ --exclude=/build/ --pretty .
+fi
 
 python3 -m flake8 . --count --show-source --statistics --exclude=__init__.py,.env,external --ignore=E501,E722,E402,W503,E203
 
 cd -
+
+echo "Finished checking"
