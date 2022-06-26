e3fba37fdb527e2f4ec5e186b481a2177e0affe2
Andras Schmelczer <andras@schmelczer.dev>
2022-06-26 12:08:19 +0200

    Improve get_sentences

--- src/great_ai/utilities/__init__.py
+++ src/great_ai/utilities/__init__.py
@@ -1,10 +1,10 @@
 from .clean import clean
-from .parallel_map import parallel_map
-from .unique import unique
 from .config_file import ConfigFile, ParseError
+from .evaluate_ranking import evaluate_ranking
 from .get_sentences import get_sentences
 from .language import english_name_of_language, is_english, predict_language
 from .lemmatize import lemmatize
 from .logger import get_logger
-from .evaluate_ranking import evaluate_ranking
 from .match_names import match_names
+from .parallel_map import parallel_map
+from .unique import unique
--- tests/utilities/test_get_sentences.py
+++ tests/utilities/test_get_sentences.py
@@ -11,6 +11,48 @@ class TestGetSentences(unittest.TestCase):
         assert get_sentences(text) == expected
         assert get_sentences(text, ignore_partial=True) == expected[0:2]
 
+    def test_complex(self) -> None:
+        text = """
+        This is a complete sentence. So is this.
+        End of paragraph.
+
+
+        Negation contractions (like don't or ain't) are resolved.
+
+        However this is not a sent
+        """
+
+        expected = [
+            "This is a complete sentence.",
+            "So is this.",
+            "End of paragraph.",
+            "Negation contractions (like don't or ain't) are resolved.",
+            "However this is not a sent",
+        ]
+
+        print(get_sentences(text, ignore_partial=True))
+
+        assert get_sentences(text) == expected
+        assert get_sentences(text, ignore_partial=True) == expected[:-1]
+
+    def test_true_casing(self) -> None:
+        text = "This is also referred to as a Convolutional Neural Network (CNN)."
+        expected = ["this is also referred to as a Convolutional Neural Network (CNN)."]
+
+        assert get_sentences(text, true_case=True) == expected
+
+    def test_remove_punctuation(self) -> None:
+        text = "Also, we --- the authors --- have to find less intrusive, and higher potential procedures. "
+        expected = [
+            "Also we the authors have to find less intrusive and higher potential procedures"
+        ]
+
+        assert get_sentences(text, remove_punctuation=True) == expected
+
     def test_empty(self) -> None:
         assert get_sentences("") == []
+        assert get_sentences(" ") == []
+        assert get_sentences("  \n ") == []
         assert get_sentences("", ignore_partial=True) == []
+        assert get_sentences("", true_case=True) == []
+        assert get_sentences("", remove_punctuation=True) == []
