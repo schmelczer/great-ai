a20a1b85bb687f1689fb5358eda1b6c3325398f8
Andras Schmelczer <andras@schmelczer.dev>
2022-06-28 18:27:22 +0200

    Make colors optional

--- src/great_ai/utilities/logger/get_logger.py
+++ src/great_ai/utilities/logger/get_logger.py
@@ -6,7 +6,9 @@ from .custom_formatter import CustomFormatter
 loggers: Dict[str, logging.Logger] = {}
 
 
-def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+def get_logger(
+    name: str, level: int = logging.INFO, disable_colors: bool = False
+) -> logging.Logger:
     if name not in loggers:
         logger = logging.getLogger(name)
         logger.setLevel(level)
@@ -14,8 +16,9 @@ def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
         fmt = "%(asctime)s | %(levelname)8s | %(message)s"
 
         stdout_handler = logging.StreamHandler()
-        stdout_handler.setLevel(logging.DEBUG)
-        stdout_handler.setFormatter(CustomFormatter(fmt))
+        stdout_handler.setLevel(level)
+        if not disable_colors:
+            stdout_handler.setFormatter(CustomFormatter(fmt))
 
         logger.addHandler(stdout_handler)
         loggers[name] = logger
