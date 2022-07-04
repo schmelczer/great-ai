286d2c0893c9fc77ab56b5dbec09c93f9473ac4d
Andras Schmelczer <andras@schmelczer.dev>
2022-07-04 09:28:29 +0200

    Show model version on dashboard

--- src/great_ai/great_ai/deploy/routes/dashboard/get_description.py
+++ src/great_ai/great_ai/deploy/routes/dashboard/get_description.py
@@ -4,26 +4,29 @@ from ....helper import snake_case_to_text, strip_lines
 
 
 def get_description(
-    function_name: str, function_docs: str, accent_color: str
+    function_name: str, version: str, function_docs: str, accent_color: str
 ) -> html.Div:
     return html.Div(
         [
             html.H1(
-                f"{snake_case_to_text(function_name)} - dashboard",
+                [
+                    f"{snake_case_to_text(function_name)} - dashboard",
+                    html.Span(version, className="version-tag"),
+                ],
                 style={"color": accent_color},
             ),
             dcc.Markdown(
                 strip_lines(
                     f"""
-                > View the live data of your deployment here.
+                    > View the live data of your deployment here.
 
-                ## Using the API
+                    ## Using the API
 
-                You can find the available endpoints at [/docs](/docs).
+                    You can find the available endpoints at [/docs](/docs).
 
-                ## Details
+                    ## Details
 
-                {function_docs}
+                    {function_docs}
                 """
                 ),
                 className="description",
