68c7da38b84b74bbee6e6b74e2dc538eea7a822c
Andras Schmelczer <andras@schmelczer.dev>
2022-07-11 19:32:45 +0200

    Remove timestamp from logs in notebooks

--- great_ai/utilities/logger/get_logger.py
+++ great_ai/utilities/logger/get_logger.py
@@ -9,13 +9,22 @@ loggers: Dict[str, logging.Logger] = {}
 def get_logger(
     name: str, level: int = logging.INFO, disable_colors: bool = False
 ) -> logging.Logger:
-    """Return a customised logger used throughout the GreatAI codebase."""
+    """Return a customised logger used throughout the GreatAI codebase.
+
+    Uses colors, and only prints timestamps when not running inside notebook.
+    """
 
     if name not in loggers:
         logger = logging.getLogger(name)
         logger.setLevel(level)
 
-        log_format = "%(asctime)s | %(levelname)8s | %(message)s"
+        try:
+            get_ipython()  # type: ignore
+            log_format = "%(message)s"
+        except NameError:
+            # will fail outside of a notebook https://ipython.org/
+            log_format = "%(asctime)s | %(levelname)8s | %(message)s"
+
         stdout_handler = logging.StreamHandler()
         stdout_handler.setLevel(level)
         if not disable_colors:
