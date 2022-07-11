96eb0e0e4ae96dfc8933cabf1f22162864b85fc9
Andras Schmelczer <andras@schmelczer.dev>
2022-07-11 14:10:47 +0200

    Remove favicon from schema

--- great_ai/deploy/routes/bootstrap_dashboard.py
+++ great_ai/deploy/routes/bootstrap_dashboard.py
@@ -14,8 +14,8 @@ PATH = Path(__file__).parent.resolve()
 def bootstrap_dashboard(app: FastAPI, function_name: str, documentation: str) -> None:
     dash_app = create_dash_app(function_name, app.version, documentation)
 
-    @app.get("/favicon.ico")
-    @app.get(f"{DASHBOARD_PATH}/_favicon.ico")
+    @app.get("/favicon.ico", include_in_schema=False)
+    @app.get(f"{DASHBOARD_PATH}/_favicon.ico", include_in_schema=False)
     def get_favicon() -> FileResponse:
         return FileResponse(PATH / "dashboard/assets/favicon.ico")
 
